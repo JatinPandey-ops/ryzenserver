9171df7ab8858e98c18dc9a7ce6601073c6dfa1b
Jatin <pandeyjatin7788@gmail.com>
2023-03-11 04:06:05 +0530

    test

--- controllers/facebookControllers.js
+++ controllers/facebookControllers.js
@@ -1,12 +1,62 @@
 import axios from "axios";
+import { arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { Configuration, OpenAIApi } from "openai"
+import { db } from "../firebase.js";
 
 export const getWebhook = async (req, res) => {
     console.log(`\u{1F7EA} Received webhook:`);
     if (req.body.object === "page") {
-   
-        res.status(200).json("received")
-        console.log(req.body)
+        try {
+            
+            const webhook_event = req.body.entry[0]
+            const psid = webhook_event.sender.id
+            const text = webhook_event.message.text
+            await setDoc(doc(db, "conversations", `${psid}@facebook.com` ), {
+                messages: [
+                    {role: "system", content: "You are a helpful assistant."},
+                    {role: "user", content: "Introduce yourself to others as Ryzen."},
+                ],
+            });
+            const userText = {
+                role :'user',
+                content:text
+            }
+            res.status(200).json("received")
+            const docRef = doc(db, "conversations",`${psid}@facebook.com`);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                
+            await updateDoc(doc(db, "conversations", `${psid}@facebook.com`), {
+                
+                messages: arrayUnion(userText)
+              });
+             const message = await docSnap.data().messages
+            const configuration = new Configuration({
+              apiKey: process.env.OPENAI_API_KEY,
+            });
+            const openai = new OpenAIApi(configuration);
+            
+            const completion = await openai.createChatCompletion({
+              model: "gpt-3.5-turbo",
+              messages : message
+            });
+            const response = completion.data.choices[0].message
+            const resData = {
+                role: response.role,
+                content: response.content
+            }
+            await axios.post(`https://graph.facebook.com/v16.0/105376291989178/messages?recipient={id:${psid}}&message={text:'${response.content}'}&messaging_type=RESPONSE&access_token=${process.env.FB_VERIFY_TOKEN}`)
+            await updateDoc(doc(db, "conversations", `${psid}@facebook.com`), {
+          
+              messages: arrayUnion(resData)
+            });
+        } else {
+            //....
+        }
+        } catch (error) {
+            console.log(error)
+        }
+
   
     } else {
       res.status(404);
@@ -26,37 +76,3 @@ export const getWebhook = async (req, res) => {
 
 
 
-
-
-
-//   const response = await axios.get(`https://graph.facebook.com/v16.0/${process.env.FB_PAGE_ID}/conversations?fields=participants&access_token=${process.env.VERIFY_TOKEN}`)
-//   const resData = response.data.data
-//   resData.forEach(object => {
-//       const convoId = object.id
-//       const psid = object.participants.data[0].id
-//       const fetchChats = async () => {
-//           const fetch = await axios.get(`https://graph.facebook.com/v16.0/${convoId}?fields=messages{message}&access_token=${process.env.VERIFY_TOKEN}`)
-//           const chat = fetch.data.messages.data
-//           let counter = 0
-//           const message = chat.map((msg) => {
-//               const role = counter % 2 === 0? "user" : "asistant";
-//               counter ++;
-//               return {
-//                   role,
-//                   message: msg.message
-//               }
-//           })
-//           console.log(message)
-//           const configuration = new Configuration({
-//               apiKey: process.env.OPENAI_API_KEY,
-//             });
-//             const openai = new OpenAIApi(configuration);
-            
-//             const completion = await openai.createChatCompletion({
-//               model: "gpt-3.5-turbo",
-//               messages : message
-//             });
-//             const botRes = completion.data.choices[0].message
-//       }
-//       fetchChats()
-//   });
