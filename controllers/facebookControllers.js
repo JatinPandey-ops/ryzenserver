86f711308af5c4aaca80f3c11fdf718fa65f403f
Jatin <pandeyjatin7788@gmail.com>
2023-03-11 20:49:13 +0530

    fixed bugs

--- controllers/facebookControllers.js
+++ controllers/facebookControllers.js
@@ -8,55 +8,56 @@ export const getWebhook = async (req, res) => {
   if (req.body.object === "page") {
     try {
       
-          res.status(200).json("Received")
-          console.log(req.body)
-          await setDoc(doc(db, "conversations","test" ), {
-                   data : req.body
-                });
-        //     const webhook_event = req.body.entry[0]
-        //     const psid = webhook_event.id
-        //     const text = webhook_event.message.text
-        //     await setDoc(doc(db, "conversations", `${psid}@facebook.com` ), {
-        //         messages: [
-        //             {role: "system", content: "You are a helpful assistant."},
-        //             {role: "user", content: "Introduce yourself to others as Ryzen."},
-        //         ],
-        //     });
-        //     const userText = {
-        //         role :'user',
-        //         content:text
-        //     }
-        //     const docRef = doc(db, "conversations",`${psid}@facebook.com`);
-        //     const docSnap = await getDoc(docRef);
-        //     if (docSnap.exists()) {
-                
-        //     await updateDoc(doc(db, "conversations", `${psid}@facebook.com`), {
-                
-        //         messages: arrayUnion(userText)
-        //       });
-        //      const message = await docSnap.data().messages
-        //     const configuration = new Configuration({
-        //       apiKey: process.env.OPENAI_API_KEY,
-        //     });
-        //     const openai = new OpenAIApi(configuration);
-            
-        //     const completion = await openai.createChatCompletion({
-        //       model: "gpt-3.5-turbo",
-        //       messages : message
-        //     });
-        //     const response = completion.data.choices[0].message
-        //     const resData = {
-        //         role: response.role,
-        //         content: response.content
-        //     }
-        //     await axios.post(`https://graph.facebook.com/v16.0/105376291989178/messages?recipient={id:${psid}}&message={text:'${response.content}'}&messaging_type=RESPONSE&access_token=${process.env.FB_VERIFY_TOKEN}`)
-        //     await updateDoc(doc(db, "conversations", `${psid}@facebook.com`), {
+      await setDoc(doc(db, "conversations","test" ), {
+        data : req.body
+      });
+      const webhook_event = req.body.entry[0]
+      const psid = webhook_event.messaging[0].sender.psid
+      const text = webhook_event.messaging[0].text
+      console.log(psid)
+      console.log(text)
+      await setDoc(doc(db, "conversations", `${psid}@facebook.com` ), {
+        messages: [
+          {role: "system", content: "You are a helpful assistant."},
+          {role: "user", content: "Introduce yourself to others as Ryzen."},
+        ],
+      });
+      const userText = {
+        role :'user',
+        content:text
+      }
+      const docRef = doc(db, "conversations",`${psid}@facebook.com`);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        
+        await updateDoc(doc(db, "conversations", `${psid}@facebook.com`), {
           
-        //       messages: arrayUnion(resData)
-        //     });
-        // } else {
-        //     //....
-        // }
+          messages: arrayUnion(userText)
+        });
+        res.status(200).json("Received")
+        const message = await docSnap.data().messages
+        const configuration = new Configuration({
+          apiKey: process.env.OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        
+        const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages : message
+        });
+        const response = completion.data.choices[0].message
+        const resData = {
+          role: response.role,
+          content: response.content
+        }
+        await axios.post(`https://graph.facebook.com/v16.0/105376291989178/messages?recipient={id:${psid}}&message={text:'${response.content}'}&messaging_type=RESPONSE&access_token=${process.env.FB_VERIFY_TOKEN}`)
+        await updateDoc(doc(db, "conversations", `${psid}@facebook.com`), {
+          
+              messages: arrayUnion(resData)
+            });
+        } else {
+            console.log("something went wrong")
+        }
         } catch (error) {
             console.log(error)
         }
