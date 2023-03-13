b41c4a841eaed1624093fb4fb2ea35086027063e
Jatin <pandeyjatin7788@gmail.com>
2023-03-13 16:40:49 +0530

    minor change

--- controllers/facebookControllers.js
+++ controllers/facebookControllers.js
@@ -9,13 +9,13 @@ export const getWebhook = async (req, res) => {
   console.log(`\u{1F7EA} Received webhook:`);
   if (req.body.object === "page") {
     try {
-      // const webhook_event = req.body.entry[0];
-      // const psid = webhook_event.messaging[0].sender.id;
-      // const text = webhook_event.messaging[0].message.text;
-      // console.log(psid);
-      // console.log(text);
-      const psid = 4835495889835312
-      const text = "hii"
+      const webhook_event = req.body.entry[0];
+      const psid = webhook_event.messaging[0].sender.id;
+      const text = webhook_event.messaging[0].message.text;
+      console.log(psid);
+      console.log(text);
+      // const psid = 4835495889835312
+      // const text = "hii"
    
       const userText = {
         uid: uuid(),
